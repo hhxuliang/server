7289497c367124c2aca3409c2f065fd5e040c072
gzhyun <gzhyun@gmail.com>
2014-10-27 22:15:38 +0800

    分离出2前后台都要用到的共享类

---
+++ Share/src/com/way/chat/common/bean/AddNewFriendMsg.java
@@ -0,0 +1,39 @@
+package com.way.chat.common.bean;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * ÃÌº”–¬≈Û”—
+ * 
+ * @author liang
+ */
+public class AddNewFriendMsg implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String userID;
+	private List<String> newFriends;
+
+	public AddNewFriendMsg() {
+		newFriends = new ArrayList<String>();
+	}
+
+	public String getUserID() {
+		return userID;
+	}
+
+	public void setUserID(String id) {
+		this.userID = id;
+	}
+	
+	public void addNewFriendID(String id) {
+		this.newFriends.add(id);
+	}
+	
+	public List<String> getFriends(){
+		return newFriends;
+	}
+}
